c3c1095b65c1830b6aa640e1182b714ce9585c10
jyodesh10 <jyodeshshakya@gmail.com>
2025-01-20 22:36:47 +0545

    api integrations

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -2,6 +2,7 @@
 
 
 import 'package:findmyguide/constants/url_constants.dart';
+import 'package:findmyguide/models/blogs_model.dart';
 import 'package:findmyguide/models/guides_model.dart';
 import 'package:findmyguide/models/tours_model.dart';
 import 'package:findmyguide/utils/http_handler.dart';
@@ -15,8 +16,12 @@ class HomeController extends GetxController{
 
   var toursList = <ToursModel>[].obs;
   var guidesList = <GuidesModel>[].obs;
+  var blogsList = <BlogData>[].obs;
+
+  var loading = false.obs;
 
   Future getToursRequest() async {
+    loading(true);
     List data = await handleRequest(
       method: "get", 
       url: "${baseUrl}api/tours", 
@@ -24,7 +29,7 @@ class HomeController extends GetxController{
         "Content-Type":"application/json",
         "Authorization": "Bearer ${SharedPref.read("accessToken")}"
       },
-    );
+    ).whenComplete(() => loading(false));
     
     toursList.value = data.map((e) => ToursModel.fromJson(e)).toList();
 
@@ -32,6 +37,7 @@ class HomeController extends GetxController{
 
 
   Future getGuidesRequest() async {
+    loading(true);
     List data = await handleRequest(
       method: "get", 
       url: "${baseUrl}api/guides", 
@@ -39,10 +45,41 @@ class HomeController extends GetxController{
         "Content-Type":"application/json",
         "Authorization": "Bearer ${SharedPref.read("accessToken")}"
       },
-    );
+    ).whenComplete(() => loading(false));
     
     guidesList.value = data.map((e) => GuidesModel.fromMap(e)).toList();
 
+  }
+
+
+  Future getBlogsRequest() async {
+    loading(true);
+    var data = await handleRequest(
+      method: "get", 
+      url: "${baseUrl}api/blog/?limit=5&page=1", 
+      headers: {
+        "Content-Type":"application/json",
+        "Authorization": "Bearer ${SharedPref.read("accessToken")}"
+      },
+    ).whenComplete(() => loading(false));
+    List listdata = data['data'];
+    blogsList.value = listdata.map((e) => BlogData.fromMap(e)).toList();
+
+  }
+
+  late BlogData blog;
+  var blogloading = false.obs;
+  Future getBlogById(id) async {
+    blogloading(true);
+    var data = await handleRequest(
+      method: "get", 
+      url: "${baseUrl}api/blog/$id", 
+      headers: {
+        "Content-Type":"application/json",
+        "Authorization": "Bearer ${SharedPref.read("accessToken")}"
+      },
+    ).whenComplete(() => blogloading(false));
+    blog = BlogData.fromMap(data);
   }
 
 }
